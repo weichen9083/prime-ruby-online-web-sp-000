12a5cdf6c129ddbf0bdda58737f01158e8ba8dd1
wei chen <weichen9083@gmail.com>
2019-09-06 00:17:00 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(interger)
+  if interger <=1
+    return false 
+    elsif (2..interger - 1).each do |i|
+      if interger % i == 0
+      return false
+    end
+  end 
+end 
+true 
+
+end 
